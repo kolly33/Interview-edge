5421ef7ed548224948771bdc05eb0b52ba3c6ccc
kola <igbalayekay99@gmail.com>
2024-12-09 16:18:19 +0100

    adding clientID

--- src/auth/strategies/auth0.strategy.ts
+++ src/auth/strategies/auth0.strategy.ts
@@ -35,7 +35,7 @@ export class Auth0Strategy extends PassportStrategy(Strategy, 'auth0') {
   constructor(private configService: ConfigService) {
     super({
       domain:configService.get<string>('AUTH0_DOMAIN'), // e.g., 'your-domain.auth0.com'
-      clientID: configService.get<string>('AUTH0_CLIENT_SECRET'),
+      clientID: configService.get<string>('CLIENT_ID'),
       clientSecret:configService.get<string>('AUTH0_CLIENT_SECRET'),
       callbackURL: configService.get<string>('AUTH0_CALLBACK_URL'), // e.g., 'http://localhost:3000/auth/callback'
     });
