f2a2886d55f46cad5430bfce351b950d1fb3cd0e
kola <igbalayekay99@gmail.com>
2024-12-09 16:51:00 +0100

    add controllermodule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,12 @@ import { PassportModule } from '@nestjs/passport';
 import { Auth0Strategy } from './strategies/auth0.strategy';
 
 import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
 
 @Module({
   imports: [PassportModule.register({ defaultStrategy: 'auth0' })],
   providers: [AuthService, Auth0Strategy],
+  controllers: [AuthController],
   exports: [AuthService],
 })
 export class AuthModule {}
